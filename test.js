b0761f78fde3481bc6a774e12ea671ac417e2cdb
Artem Borisov <ba@maxx-marketing.net>
2019-07-10 16:38:35 +0300

    tests

--- test.js
+++ test.js
@@ -122,7 +122,7 @@ todoApp.promise.then(function(){
     });
     
     describe("getKeyForId", function() {
-        let positions = [-1, 0, Math.round(todoApp.todosData.length * 0.5), todoApp.todosData.length - 1];
+        let positions = [-1, 0, Math.round(todoApp.todosData.length * 0.5), todoApp.todosData.length - 1, todoApp.todosData.length];
         positions.forEach(function(pos, index, positions) {
             let result = false;
             let id = -1;
@@ -141,7 +141,7 @@ todoApp.promise.then(function(){
     });
     
     describe("deleteItem", function() {
-        let positions = [-1, 0, Math.round(todoApp.todosData.length * 0.5), todoApp.todosData.length - 1];
+        let positions = [-1, 0, Math.round(todoApp.todosData.length * 0.5), todoApp.todosData.length - 1, todoApp.todosData.length];
         positions.forEach(function(pos, index, positions) {
             let length = todoApp.todosData.length;
             if ((pos >= 0) && (pos < todoApp.todosData.length)) length--;
@@ -157,11 +157,49 @@ todoApp.promise.then(function(){
                 counter++;
             });
             
-            
             it("delete item for id = " + id + " on position: " + position, function() {
                 let copy = todoApp.todosData.slice();
                 todoApp.deleteItem(id);
                 assert((todoApp.getKeyForId(id) === false) && (todoApp.todosData.length === length));
+                todoApp.todosData = copy.slice();
+            });
+        })
+    });
+    
+    describe("updateState", function() {
+        let positions = [0, Math.round(todoApp.todosData.length * 0.5), todoApp.todosData.length - 1];
+        positions.forEach(function(pos, index, positions) {
+            let position = undefined;
+            let id = -1;
+            let counter = 0;
+            todoApp.todosData.forEach(function(item, i, arr) {
+                if (counter == pos) {
+                    position = i;
+                    id = item.id;
+                }
+                counter++;
+            });
+            
+            it("update state for id = " + id + " on position: " + position, function() {
+                let copy = todoApp.todosData.slice();
+                
+                let result = false;
+                let position = undefined;
+                let counter = 0;
+                todoApp.todosData.forEach(function(item, i, arr) {
+                    if (counter == pos) {
+                        position = i;
+                        result = item.completed;
+                    }
+                    counter++;
+                });
+                
+                todoApp.updateState(id);
+                if ((position >= 0) && (position < todoApp.todosData.length)) {
+                    assert(todoApp.todosData[position].completed === !result);
+                } else {
+                    assert(true);
+                }
                 todoApp.todosData = copy.slice();
             });
         })
